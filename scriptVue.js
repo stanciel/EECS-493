4a595b82c32d2ef395a7ffbc3f7213efe89c2c8a
eecs <ananyavs@umich.edu>
2021-11-17 19:11:49 -0500

    initial changes

--- scriptVue.js
+++ scriptVue.js
@@ -12,6 +12,12 @@ var user= new Vue({
           weight: 0,
           firstname: '',
           lastname: '',
+          // BMR: 0,
+
+          //events
+          eventsList: [],
+          numEvents: 0,
+          search: '',
         }
       },
       methods: {
@@ -26,42 +32,44 @@ var user= new Vue({
             console.log(this.email)
             console.log(this.firstname)
           },
-      }
-})
-var eventsInfo = new Vue({
-  el: '#app',
-  data: {
-      eventsList: [],
-      numEvents: 0,
-      search: '',
+          // calculateBMR:function() {
+          //   if (this.gender === "female") {
+          //     this.BMR = 10 * this.weight + 6.25 * this.height - 5 * this.age + 5;
+          //   }
+          //   else {
+          //     this.BMR = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161;
+          //   }
+          // }
+          submit:function() {
+            //keyword search
+            if(this.search){
+              return this.eventsList.filter((item)=>{
+                return (this.eventsList.toLowerCase().split(' ').every(v => item.name.toLowerCase().includes(v)) || this.eventsList.type.forEach(
+                  type => {type.toLowerCase().split(' ').every(v => type.toLowerCase().includes(v))}))
+              })
+            }
+          },
+          calculateCalories: function() {
+            calorieRates [{cycling: 0, walking: 0, running: 0}]; 
+            this.eventsList.forEach(element => {
+              element.type.forEach(typeElement => {
+                //search in calorie array for the type to get calorie count - array find
+                //push to event calories array
+              })
+            })
+            
+          },
+          calculateDistance: function() {
 
-  },
-  methods: {
-      submit:function() {
-        //keyword search
-        
-      },
-      calculateCalories: function() {
-        if (user.gender === "male") {
-
-        }
-          //if male
-          //BMR = 10W + 6.25H - 5A + 5
-
-          //if female
-          //BMR = 10W + 6.25H - 5A - 161
-      },
-      calculateDistance: function() {
-
-      },
-      sortEvents: function() {
-        //sort based 
-          
-      },
-      addEvents: function() {
-          //add more events
-          this.eventsList.push({name:"MVMNT Fit Cycling", type: ["cycling", "HIIT"], location:"1300 South University Avenue suite 6 , Ann Arbor MI 48104", distance:0, price: null, openSpots: 0, peopleAttending: 0, reccuring: false, calories: 0, duration: 60});
-          //update numEvents
+          },
+          sortEvents: function() {
+            //sort based 
+              
+          },
+          addEvents: function() {
+              //add more events
+              this.eventsList.push({name:"MVMNT Fit Cycling", type: ["cycling"], location:"1300 South University Avenue suite 6 , Ann Arbor MI 48104", distance:0, price: null, openSpots: 0, peopleAttending: 0, reccuring: false, calories: [0], duration: 60});
+              //update numEvents
       }
   }
 })
